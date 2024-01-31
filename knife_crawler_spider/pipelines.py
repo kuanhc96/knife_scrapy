d63e61ea8b6454015a70aebe827f6cd1dcbe80e6
kuanhc96 <kuanhc96@uchicago.edu>
2024-01-31 23:17:27 +0800

    downloaded and serialized 1000 items

--- knife_crawler_spider/pipelines.py
+++ knife_crawler_spider/pipelines.py
@@ -5,9 +5,23 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy import Request
 
+class SerializeImagesPipeline(ImagesPipeline):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.counter = 0
+    
+    def get_media_requests(self, item, info): # order of execution: 1st
+        for image_url in item.get('image_urls', []):
+            yield Request(image_url)
 
-class KnifeCrawlerSpiderPipeline:
-    def process_item(self, item, spider):
+    def file_path(self, request, response=None, info=None): # order of execution: 2nd
+        filename = f"{str( self.counter ).zfill(4)}.jpg"
+        self.counter += 1
+        return filename
+
+    def item_completed(self, results, item, info): # order of execution: 3rd
+        item['images'] = [image for success, image in results if success]
         return item
