0389b61eee3fa8206144bb9c5439a1dc67c9a2f8
kuanhc96 <kuanhc96@uchicago.edu>
2024-02-02 00:38:07 +0800

    implement serialized file names

--- knife_crawler_spider/spiders/knife_spider.py
+++ knife_crawler_spider/spiders/knife_spider.py
@@ -1,11 +1,11 @@
-from knife_crawler_spider.items import KnifeCrawlerSpiderItem
-import datetime
+from knife_crawler_spider.items import KnifeImageItem
 import scrapy
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-# from production.items import ProductionItem, ImageItems
 
 class KnifeSpider(scrapy.Spider):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.counter = 0
+
     name = "knife_spider"
     allowed_domains = ["seisukeknife.com"]
     start_urls = ["https://int.seisukeknife.com/collections/gyuto-chefs-knife"]
@@ -15,9 +15,8 @@ class KnifeSpider(scrapy.Spider):
         for url in  urls:
             url = response.urljoin(url)
             url = url.replace("{width}", "440")
-            yield {
-                "image_urls" : [url],
-            }
+            KnifeImageItem(image_url=url, title=self.counter)
+            self.counter += 1
         
         next_page = response.css("div div.main-content div.index-wrapper div.collection-wrapper section div div main div div.pagination-next a::attr(href)").extract_first()
         if next_page:
