8a3e83fe86521c75db7fbfbbfa27bc8419318eea
kuanhc96 <kuanhc96@uchicago.edu>
2024-02-02 00:27:32 +0800

    use counter to name files

--- knife_crawler_spider/pipelines.py
+++ knife_crawler_spider/pipelines.py
@@ -9,19 +9,22 @@ from scrapy.pipelines.images import ImagesPipeline
 from scrapy import Request
 
 class SerializeImagesPipeline(ImagesPipeline):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.counter = 0
     
     def get_media_requests(self, item, info): # order of execution: 1st
-        for image_url in item.get('image_urls', []):
-            yield Request(image_url)
+        # for image_url in item.get('image_urls', []):
+        image_url = item.get('image_url')
+        title = item.get('title', 'XXXXX')
+        yield Request(image_url, meta={'title': title})
 
     def file_path(self, request, response=None, info=None): # order of execution: 2nd
-        filename = f"{str( self.counter ).zfill(4)}.jpg"
-        self.counter += 1
+        title = request.meta.get('title', 'XXXXX')
+        filename = f"{str( title ).zfill(4)}.jpg"
         return filename
 
     def item_completed(self, results, item, info): # order of execution: 3rd
-        item['images'] = [image for success, image in results if success]
+        results = results[0]
+        success = results[0]
+        image = results[1]
+        if success:
+            item['image'] = image 
         return item
