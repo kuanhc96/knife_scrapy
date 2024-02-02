bde356485d1905d207c340c0ee623f13f8ec8992
kuanhc96 <kuanhc96@uchicago.edu>
2024-02-02 17:29:00 +0800

    get knife type during crawl

--- knife_crawler_spider/spiders/knife_spider.py
+++ knife_crawler_spider/spiders/knife_spider.py
@@ -15,10 +15,12 @@ class KnifeSpider(scrapy.Spider):
     
     def parse(self, response):
         urls = response.css("div.product-item__wrapper div.product-item__image-wrapper div.height-inherit.has-secondary-image").css("a img::attr(data-src)").getall()
+        knife_type_full = response.css("div div div div div section div div div h2::text").extract_first().split(" ")
+        knife_type = knife_type_full[0].lower()
         for url in  urls:
             url = response.urljoin(url)
             url = url.replace("{width}", "440")
-            yield KnifeImageItem(image_url=url, title=self.counter)
+            yield KnifeImageItem(image_url=url, title=self.counter, knife_type=knife_type)
             self.counter += 1
         
         next_page = response.css("div div.main-content div.index-wrapper div.collection-wrapper section div div main div div.pagination-next a::attr(href)").extract_first()
