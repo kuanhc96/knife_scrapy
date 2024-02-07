b9f1d0485d4e5937ae2b66a358f817c35492ffc3
kuanhc96 <kuanhc96@uchicago.edu>
2024-02-07 16:05:59 +0800

    scrape santoku knives from japanesechefsknife.com

--- knife_crawler_spider/spiders/knife_spider.py
+++ knife_crawler_spider/spiders/knife_spider.py
@@ -6,38 +6,36 @@ import scrapy
 class KnifeSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.page = 1
 
     # Spider name
     name = "knife_spider"
 
     # List of allowed domains to scrape
-    allowed_domains = ["musashihamono.com"]
+    allowed_domains = ["japanesechefsknife.com"]
 
     # List of starting URLs for the spider
     start_urls = [
-        "https://musashihamono.com/collections/japanese-chef-knife-gyuto-series",
-        "https://musashihamono.com/collections/japanese-chef-knife-santoku-series",
-        "https://musashihamono.com/collections/japanese-chef-knife-nakiri-series",
-        "https://musashihamono.com/collections/japanese-chef-knife-deba-series",
+        "https://japanesechefsknife.com/collections/wa-santoku",
     ]
 
     # Parse method for handling the response
     def parse(self, response):
         # Extract image URLs
-        urls = response.css("div div main div div div div div div div div div div div div a div div img::attr(data-src)").getall()
+        urls = response.css("div div div div div a div img::attr(data-src)").getall()
 
         # Extract the knife type from the response
-        knife_type_full = response.css("div div main div div div h2::text").extract_first()
-        knife_type = knife_type_full.lower()
+        knife_type_full = response.css("div div div div h1 a::text").extract_first()
+        knife_type = knife_type_full.split()[1].lower()
 
         # Iterate through image URLs and yield KnifeImageItem
         for url in urls:
             url = response.urljoin(url)
-            url = url.replace("{width}", "540")
+            url = url.replace("{width}", "380")
             yield KnifeImageItem(image_url=url, knife_type=knife_type)
 
         # Extract the URL of the next page
-        next_page = response.css("div div main div div div div div div div div div span.next a::attr(href)").extract_first()
+        next_page = response.css("div div div div div span.next a::attr(href)").extract_first()
 
         # Check if there is a next page and send a request to it
         if next_page:
