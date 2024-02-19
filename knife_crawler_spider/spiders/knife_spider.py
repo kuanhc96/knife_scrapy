19ffca5ef913cd855b083463ae7d857e81bfdd5e
kuanhc96 <kuanhc96@uchicago.edu>
2024-02-19 13:35:41 +0800

    scraped chuboknives

--- knife_crawler_spider/spiders/knife_spider.py
+++ knife_crawler_spider/spiders/knife_spider.py
@@ -6,36 +6,35 @@ import scrapy
 class KnifeSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.page = 1
 
     # Spider name
     name = "knife_spider"
 
     # List of allowed domains to scrape
-    allowed_domains = ["japanesechefsknife.com"]
+    allowed_domains = ["chubokuknives.com"]
 
     # List of starting URLs for the spider
     start_urls = [
-        "https://japanesechefsknife.com/collections/wa-santoku",
+        "https://www.chuboknives.com/collections/santoku",
     ]
 
     # Parse method for handling the response
     def parse(self, response):
         # Extract image URLs
-        urls = response.css("div div div div div a div img::attr(data-src)").getall()
+        urls = response.css("div div div div div div div div div img::attr(data-src)").getall()
 
         # Extract the knife type from the response
-        knife_type_full = response.css("div div div div h1 a::text").extract_first()
-        knife_type = knife_type_full.split()[1].lower()
+        knife_type_full = response.css("div div div div div h1::text").extract_first()
+        knife_type = knife_type_full.split()[0].lower()
 
         # Iterate through image URLs and yield KnifeImageItem
         for url in urls:
             url = response.urljoin(url)
-            url = url.replace("{width}", "380")
+            url = url.replace("{width}", "720")
             yield KnifeImageItem(image_url=url, knife_type=knife_type)
 
         # Extract the URL of the next page
-        next_page = response.css("div div div div div span.next a::attr(href)").extract_first()
+        next_page = None #response.css("div div div div div span.next a::attr(href)").extract_first()
 
         # Check if there is a next page and send a request to it
         if next_page:
